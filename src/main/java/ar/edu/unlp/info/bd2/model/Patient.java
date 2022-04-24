8002d936e913421875001e57501f852db081c7f8
Santiago Ponte <saponteahon@gmail.com>
2022-04-24 20:20:59 -0300

    Intentando usar anotations de Hibernate

--- src/main/java/ar/edu/unlp/info/bd2/model/Patient.java
+++ src/main/java/ar/edu/unlp/info/bd2/model/Patient.java
@@ -1,6 +1,12 @@
 package ar.edu.unlp.info.bd2.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity(name = "Patient")
 public class Patient {
+
+    @Id
     private int id;
     private String name;
     private VaccinationSchedule schedule;
