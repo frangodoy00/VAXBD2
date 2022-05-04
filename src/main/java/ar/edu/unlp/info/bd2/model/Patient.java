1ae4738bbf2b7c3df84638f17dcebe309768b9f9
Santiago Ponte <saponteahon@gmail.com>
2022-05-04 13:06:34 -0300

    Agregando atributos y metodos necesarios para los tests a la clase Patient

--- src/main/java/ar/edu/unlp/info/bd2/model/Patient.java
+++ src/main/java/ar/edu/unlp/info/bd2/model/Patient.java
@@ -1,6 +1,9 @@
 package ar.edu.unlp.info.bd2.model;
 
+import sun.util.calendar.BaseCalendar;
+
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name = "Patient")
@@ -11,8 +14,17 @@ public class Patient {
     @Column(name="Id", unique = true, nullable = false)
     private int id;
 
-    @Column(name="Name", nullable = false)
-    private String name;
+    @Column(name="Email", nullable = false)
+    private String email;
+
+    @Column(name="Fullname", nullable = false)
+    private String fullname;
+
+    @Column(name="Password", nullable = false)
+    private String password;
+
+    @Column(name="DayOfBirth", nullable = false)
+    private Date dayOfBirth;
 
     @ManyToOne(fetch = FetchType.EAGER)
     private VaccinationSchedule schedule;
@@ -21,16 +33,27 @@ public class Patient {
 
     }
 
-    public Patient(String name, VaccinationSchedule schedule){
-        this.name = name;
-        this.schedule = schedule;
+    public Patient(String email, String fullname, String password, Date dayOfBirth){
+        this.email = email;
+        this.fullname = fullname;
+        this.password = password;
+        this.dayOfBirth = dayOfBirth;
     }
 
     public int getId() { return this.id; }
     public void setId(int id) { this.id = id; }
 
-    public String getName() { return this.name; }
-    public void setName(String name) { this.name = name; }
+    public String getEmail() { return this.email; }
+    public void setEmail(String name) { this.email = email; }
+
+    public String getFullname() { return this.fullname; }
+    public void setFullname(String fullname) { this.fullname = fullname; }
+
+    public String getPassword() { return this.password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public Date getDayOfBirth() { return this.dayOfBirth; }
+    public void setDayOfBirth(Date dayOfBirth) { this.dayOfBirth = dayOfBirth; }
 
     public VaccinationSchedule getSchedule() {
         return this.schedule;
